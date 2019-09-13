e177ea9329f496365bda5c417551cede4dd0930f
Andrés Vázquez <andres@data99.com.ar>
2019-09-13 20:48:39 -0300

    práctico 2 procesado OK

---
+++ practico2/start.sh
@@ -0,0 +1,6 @@
+# ger X13 and lib
+wget -O get-x13as.sh https://github.com/DiploDatos/AnalisisSeriesTemporales/raw/master/get-x13as.sh
+sh get-x13as.sh
+
+# lo agregué al repo
+# wget -O plotting.py https://github.com/DiploDatos/AnalisisSeriesTemporales/raw/master/plotting.py
